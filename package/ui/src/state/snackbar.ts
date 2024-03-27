a97d10ff152953be90ae4e3e9dcc201b671e3e2e
jules <jules.leidelinger@laplace-research.com>
2024-03-27 17:56:04 +0800

    change

--- package/ui/src/state/snackbar.ts
+++ package/ui/src/state/snackbar.ts
@@ -1,6 +1,6 @@
 import { atom } from "recoil";
 
-export const snackbarState = atom<{ message: string }>({
+export const snackbarState = atom<{ message: string; type?: 'error' | 'success' | 'info' | 'warning' } | null>({
   key: "snackbarState",
-  default: undefined,
+  default: null,
 });
