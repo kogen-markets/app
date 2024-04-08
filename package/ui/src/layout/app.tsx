e08ec75387d8f28d08250d43327adf5dc99b159c
jules <jules.leidelinger@laplace-research.com>
2024-04-08 21:19:35 +0800

    change address of factory and order contracts by descending date

--- package/ui/src/layout/app.tsx
+++ package/ui/src/layout/app.tsx
@@ -45,15 +45,17 @@ export default function App() {
   useEffect(() => {
     const callOptionsAddr = options.data
       ?.filter((v) => v.option_type === "call")
+      .sort((b, a) => Number(b.option_config.expiry) - Number(a.option_config.expiry))
       .map((v) => v.addr);
     const putOptionsAddr = options.data
       ?.filter((v) => v.option_type === "put")
+      .sort((b, a) => Number(b.option_config.expiry) - Number(a.option_config.expiry))
       .map((v) => v.addr);
     setOptionContractsAddrState((oldAddr) => ({
       ...oldAddr,
       [chain.chain_id]: {
         call: callOptionsAddr ?? [],
-        put: putOptionsAddr?.reverse() ?? [],
+        put: putOptionsAddr ?? [],
       },
     }));
   }, [options.data]);
