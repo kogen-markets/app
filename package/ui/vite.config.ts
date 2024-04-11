9afce769763941de9e89b4f36a0dab0b3f3f8c91
jules <jules.leidelinger@laplace-research.com>
2024-04-12 07:25:19 +0800

    clean up

--- package/ui/vite.config.ts
+++ package/ui/vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "react-dom": path.resolve(__dirname, "../../node_modules/react-dom"),
-    },
-  },
   plugins: [react(), nodePolyfills()],
 });
