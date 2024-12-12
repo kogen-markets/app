5c0b6b271c348b0b10e21f9ac0c6c87433d3c967
tsukipond8531 <reader2027@gmail.com>
2024-12-12 17:39:07 +0900

    update expiry component

--- package/ui/src/pages/options/components/call-form/expiry.tsx
+++ package/ui/src/pages/options/components/call-form/expiry.tsx
@@ -6,6 +6,11 @@ export default function Expiry({ config }: { config: Config }) {
   const calculateCorrectedExpiry = (expiry: string) => {
     const expiryDate = new Date(parseInt(expiry) / 1000000);
 
+    if (isNaN(expiryDate.getTime())) {
+      console.error("Invalid expiry date", expiry);
+      return new Date();
+    }
+
     if (expiryDate.getUTCHours() === 0 && expiryDate.getUTCMinutes() === 30) {
       expiryDate.setUTCDate(expiryDate.getUTCDate() + 1);
     }
@@ -13,20 +18,22 @@ export default function Expiry({ config }: { config: Config }) {
     return expiryDate;
   };
 
+  const formattedExpiry = calculateCorrectedExpiry(
+    config.expiry
+  ).toLocaleString("en-GB", {
+    month: "long",
+    day: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: "UTC",
+    timeZoneName: "short",
+  });
+
   return (
     <WithValue value={config.expiry}>
       <Typography variant="caption">Expiry</Typography>
-      <Typography variant="body1">
-        {calculateCorrectedExpiry(config.expiry).toLocaleString("en-GB", {
-          month: "long",
-          day: "2-digit",
-          year: "numeric",
-          hour: "2-digit",
-          minute: "2-digit",
-          timeZone: "UTC",
-          timeZoneName: "short",
-        })}
-      </Typography>
+      <Typography variant="body1">{formattedExpiry}</Typography>
     </WithValue>
   );
 }
