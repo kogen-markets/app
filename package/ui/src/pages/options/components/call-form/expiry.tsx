42edb9cc7684353bf5408efc10a0a2405c240e8a
tsukipond8531 <reader2027@gmail.com>
2024-12-13 00:42:05 +0900

    update expiry component(revert)

--- package/ui/src/pages/options/components/call-form/expiry.tsx
+++ package/ui/src/pages/options/components/call-form/expiry.tsx
@@ -3,37 +3,20 @@ import WithValue from "../../../../components/with-value";
 import { Config } from "../../../../codegen/KogenMarkets.types";
 
 export default function Expiry({ config }: { config: Config }) {
-  const calculateCorrectedExpiry = (expiry: string) => {
-    const expiryDate = new Date(parseInt(expiry) / 1000000);
-
-    if (isNaN(expiryDate.getTime())) {
-      console.error("Invalid expiry date", expiry);
-      return new Date();
-    }
-
-    if (expiryDate.getUTCHours() === 0 && expiryDate.getUTCMinutes() === 30) {
-      expiryDate.setUTCDate(expiryDate.getUTCDate() + 1);
-    }
-
-    return expiryDate;
-  };
-
-  const formattedExpiry = calculateCorrectedExpiry(
-    config.expiry
-  ).toLocaleString("en-GB", {
-    month: "long",
-    day: "2-digit",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    timeZone: "UTC",
-    timeZoneName: "short",
-  });
-
   return (
     <WithValue value={config.expiry}>
       <Typography variant="caption">Expiry</Typography>
-      <Typography variant="body1">{formattedExpiry}</Typography>
+      <Typography variant="body1">
+        {new Date(parseInt(config.expiry) / 1000000).toLocaleString("en-GB", {
+          month: "long",
+          day: "2-digit",
+          year: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+          timeZone: "UTC",
+          timeZoneName: "short",
+        })}
+      </Typography>
     </WithValue>
   );
 }
